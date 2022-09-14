263b08a584f74020cf89533e00d08f557dafcb79
Dragos Predatu <dragos.predatu@alfam.com>
2022-09-14 19:02:21 +0300

    added curs8

--- src/main/java/utils/BaseTest.java
+++ src/main/java/utils/BaseTest.java
@@ -13,7 +13,8 @@ public class BaseTest {
 	@BeforeClass
 	public void setup() {
 		driver = WebDriverManager.chromedriver().create();
-		driver.get("http://keybooks.ro");
+		//driver.get("http://keybooks.ro");
+		driver.get("https://the-internet.herokuapp.com/dynamic_loading/2");
 	}
 	
 	@AfterClass
